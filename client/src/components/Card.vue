f528b41e8b9cf03b8f8772f0856fffd27f935969
RieJxX <i.a.ivanitski@gmail.com>
2025-04-13 10:52:27 +0300

    card component

---
+++ client/src/components/Card.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  title: String,
+  imageUrl: String,
+})
+</script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <div class="w-50 h-50 bg-bg flex flex-col items-center pt-2 rounded-md">
+      <img class="w-4/5 rounded-md" :src="imageUrl" />
+      <p class="text-gold text-lg">{{ title }}</p>
+    </div>
+  </div>
+</template>
