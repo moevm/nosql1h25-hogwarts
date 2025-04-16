f9c263471830055209fd7510a79e97eaf7e010d2
elshaddd <elshadisaev48@fgmail.com>
2025-04-16 14:55:18 +0300

    services in db class

--- potter_api/services/character_services.py
+++ potter_api/services/character_services.py
@@ -1,39 +1,25 @@
 from models.character import Character, CharacterRelationship
-from services.database import Neo4jDatabase
 
 
-class CharacterService(Neo4jDatabase):
-    def create_character(self, name, house=None, blood_status=None, gender=None, description=None):
-        return Character(
-            name=name,
-            house=house,
-            blood_status=blood_status,
-            gender=gender,
-            description=description
-        ).save()
+class CharacterService:
+    def __init__(self, database):
+        self.db = database
 
-    def get_all_characters(self):
+    def create(self, name, **kwargs):
+        return Character(name=name, **kwargs).save()
+
+    def get_all(self):
         return Character.nodes.all()
 
     def add_relationship(self, char1_id, char2_id, rel_type):
         char1 = Character.nodes.get(id=char1_id)
         char2 = Character.nodes.get(id=char2_id)
-
-        if rel_type not in CharacterRelationship.type.choices:
-            raise ValueError(
-                f"Invalid relationship type. Allowed: {list(CharacterRelationship.type.choices.keys())}")
-
         char1.relationships.connect(char2, {'type': rel_type})
         return True
 
-    def get_character_relationships(self, char_id, rel_type=None):
+    def get_relationships(self, char_id, rel_type=None):
         char = Character.nodes.get(id=char_id)
         if rel_type:
             return [rel for rel in char.relationships.all()
                     if char.relationships.relationship(rel).type == rel_type]
         return char.relationships.all()
-
-    def get_relationship_properties(self, char_id, related_char_id):
-        char = Character.nodes.get(id=char_id)
-        related = Character.nodes.get(id=related_char_id)
-        return char.relationships.relationship(related)
--- potter_api/services/database.py
+++ potter_api/services/database.py
@@ -1,12 +1,16 @@
 from neomodel import config, db
 from config import Config
+from services.character_services import CharacterService
 
 
 class Neo4jDatabase:
     def __init__(self):
         config.DATABASE_URL = Config.DATABASE_URL
         self.db = db
+        self.characters = CharacterService(self)
 
-    def clear_all_data(self):
+    def clear_data(self):
         self.db.cypher_query("MATCH (n) DETACH DELETE n")
 
+    def execute_query(self, query, params=None):
+        return self.db.cypher_query(query, params)
