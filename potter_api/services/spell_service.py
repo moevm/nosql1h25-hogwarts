118ba73ae82e9560e3abfaa983944c317da7aee0
elshaddd <elshadisaev48@fgmail.com>
2025-04-16 15:03:00 +0300

    spell added

---
+++ potter_api/services/spell_service.py
@@ -0,0 +1,19 @@
+from models.spell import Spell
+from models.character import Character
+
+
+class SpellService:
+    def __init__(self, database):
+        self.db = database
+
+    def create(self, name, effect=None, type=None):
+        return Spell(name=name, effect=effect, type=type).save()
+
+    def get_all(self):
+        return Spell.nodes.all()
+
+    def add_knows(self, char_id, spell_id):
+        char = Character.nodes.get(id=char_id)
+        spell = Spell.nodes.get(id=spell_id)
+        char.knows.connect(spell)
+        return True
