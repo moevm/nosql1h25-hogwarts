01b64067b20552cf77f02f133445b1cd4b54d213
RieJxX <i.a.ivanitski@gmail.com>
2025-04-21 01:25:58 +0300

    fix env path

--- client/src/pages/Poison.vue
+++ client/src/pages/Poison.vue
@@ -5,7 +5,7 @@ import { useRoute } from 'vue-router'
 const items = ref([])
 
 onMounted(async () => {
-  const data = await fetch(`${import.meta.env.VITE_SERVER_URL}/poisons`, {
+  const data = await fetch(`${import.meta.env.VITE_SERVER_URL}/potions`, {
     method: 'GET'
   })
 
