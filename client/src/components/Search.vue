95982987025eb6bbd109ed77a6d933da950660d8
RieJxX <i.a.ivanitski@gmail.com>
2025-04-21 18:03:37 +0300

    add filters

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import CharacterFilters from './CharacterFilters.vue'
 import PoisonFilter from './PoisonsFilters.vue'
 import SpellFilter from './SpellFilter.vue'
@@ -6,11 +7,52 @@ import { useRoute } from 'vue-router'
 
 const route = useRoute()
 
+const options = ref([])
+const searchQuery = ref('') // Строка поиска
+
 defineProps({
   modalToggle: Function,
   placeholder: String,
   modalOpen: Boolean
 })
+
+const emit = defineEmits(['fetchUpdate'])
+
+const updateItems = async () => {
+  try {
+    const query = options.value
+      .map((el) => {
+        const key = encodeURIComponent(el.key.toLowerCase())
+        const value = encodeURIComponent(el.value.toLowerCase())
+        return `${key}=${value}`
+      })
+      .join('&')
+
+    const searchParam = searchQuery.value
+      ? `search=${encodeURIComponent(searchQuery.value.toLowerCase())}`
+      : ''
+
+    const combinedQuery = [query, searchParam].filter(Boolean).join('&')
+
+    const apiBase = import.meta.env.VITE_SERVER_URL
+    const endpointPath = route.path
+    const fullUrl = `${apiBase}/api${endpointPath}?${combinedQuery}`
+
+    console.log('Full API request:', fullUrl)
+
+    const res = await fetch(fullUrl)
+    const data = await res.json()
+
+    console.log('Received data:', data)
+    emit('fetchUpdate')
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
+
+const makeOptions = (data) => {
+  options.value = data
+}
 </script>
 
 <template>
@@ -20,9 +62,11 @@ defineProps({
         <input
           class="flex-1 pl-3 py-3 bg-bg text-gold outline-none font-display placeholder:text-[#6C6F27]"
           :placeholder="placeholder"
+          v-model="searchQuery"
         />
         <button
           class="text-bg h-14 w-14 rounded-md border border-gold flex justify-center items-center"
+          @click="updateItems"
         >
           <img class="w-6 h-6" src="/images/search.svg" />
         </button>
@@ -42,11 +86,17 @@ defineProps({
         v-if="modalOpen"
         class="bg-bg w-full h-[400px] rounded-md border border-gold absolute b-[-1px] l-0 p-6 flex gap-[300px]"
       >
-        <template v-if="route.path.includes('/character')"> <CharacterFilters /> </template>
+        <template v-if="route.path.includes('/character')">
+          <CharacterFilters @makeOptions="makeOptions" />
+        </template>
 
-        <template v-else-if="route.path.includes('/spell')"> <SpellFilter /> </template>
+        <template v-else-if="route.path.includes('/spell')">
+          <SpellFilter @makeOptions="makeOptions" />
+        </template>
 
-        <template v-else-if="route.path.includes('/potion')"> <PoisonFilter /> </template>
+        <template v-else-if="route.path.includes('/potion')">
+          <PoisonFilter @makeOptions="makeOptions" />
+        </template>
       </div>
     </transition>
   </div>
--- client/src/components/SpellFilter.vue
+++ client/src/components/SpellFilter.vue
@@ -1,28 +1,57 @@
-<script setup></script>
+<script setup>
+import { reactive, watch } from 'vue'
+
+const emit = defineEmits(['makeOptions'])
+
+const filters = reactive({
+  category: '',
+  effect: '',
+  light: '',
+  user: ''
+})
+
+watch(
+  () => filters,
+  () => {
+    console.log(filters)
+    const options = Object.entries(filters)
+      .filter(([_, value]) => value) // отбрасываем пустые
+      .map(([key, value]) => ({ key, value }))
+    emit('makeOptions', options)
+  },
+  { deep: true }
+)
+</script>
 
 <template>
-<div>
   <div class="flex flex-col flex-wrap justify-around h-full">
     <div class="text-gold">
       Category:
       <select
+        v-model="filters.category"
         class="bg-bg text-gold border border-gold rounded-md px-4 py-2 font-display outline-none focus:ring-1 focus:ring-gold ml-2"
       >
+        <option class="bg-bg text-gold" value="">--</option>
         <option class="bg-bg text-gold" value="Charm">Charm</option>
         <option class="bg-bg text-gold" value="Curse">Curse</option>
       </select>
     </div>
     <div class="text-gold">
       Effect:
-      <input class="text-gold outline-none border border-gold rounded-md ml-2" />
+      <input
+        v-model="filters.effect"
+        class="text-gold outline-none border border-gold rounded-md ml-2"
+      />
     </div>
   </div>
   <div class="flex flex-col flex-wrap justify-around h-full">
     <div class="text-gold">
       Light:
       <select
+        v-model="filters.light"
         class="bg-bg text-gold border border-gold rounded-md px-4 py-2 font-display outline-none focus:ring-1 focus:ring-gold ml-2"
       >
+        <option class="bg-bg text-gold" value="">--</option>
         <option class="bg-bg text-gold" value="Blue">Blue</option>
         <option class="bg-bg text-gold" value="Red">Red</option>
         <option class="bg-bg text-gold" value="Green">Green</option>
@@ -32,8 +61,10 @@
     </div>
     <div class="text-gold">
       User:
-      <input class="text-gold outline-none border border-gold rounded-md ml-2" />
+      <input
+        v-model="filters.user"
+        class="text-gold outline-none border border-gold rounded-md ml-2"
+      />
     </div>
-  </div>
   </div>
 </template>
