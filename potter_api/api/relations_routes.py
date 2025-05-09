2db76e4f8ca62f7ac69f7781eafe6abfe126f33f
elshaddd <elshadisaev48@gmail.com>
2025-05-09 14:10:22 +0300

    added types

--- potter_api/api/relations_routes.py
+++ potter_api/api/relations_routes.py
@@ -5,18 +5,32 @@ def register_graph_routes(app, _):
     @app.route('/api/relations/<node_type>', methods=['GET'])
     def get_relation_types(node_type):
         try:
-            query = f"""
+            query_types = f"""
             MATCH (n:`{node_type.capitalize()}`)-[r]-()
             RETURN DISTINCT type(r) AS relation_type
             """
 
-            results, _ = db.cypher_query(query)
+            type_results, _ = db.cypher_query(query_types)
+            relation_types = [row[0] for row in type_results]
 
-            relations = [row[0] for row in results]
+            flat_types = []
+
+            for rel_type in relation_types:
+                if rel_type == "HAS_RELATIONSHIP_WITH":
+                    sub_query = f"""
+                    MATCH (n:`{node_type.capitalize()}`)-[r:`{rel_type}`]-()
+                    WHERE exists(r.type)
+                    RETURN DISTINCT r.type AS subtype
+                    """
+                    subtypes_result, _ = db.cypher_query(sub_query)
+                    subtypes = [row[0] for row in subtypes_result]
+                    flat_types.extend(subtypes)
+                else:
+                    flat_types.append(rel_type)
 
             return jsonify({
                 'node_type': node_type,
-                'relation_types': relations
+                'relation_types': flat_types
             })
 
         except Exception as e:
