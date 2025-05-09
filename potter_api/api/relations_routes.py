5fb89e9e8c1887b9886cca32c26f0f06f4622a55
elshaddd <elshadisaev48@gmail.com>
2025-05-09 14:08:13 +0300

    added relations routes

---
+++ potter_api/api/relations_routes.py
@@ -0,0 +1,24 @@
+from flask import jsonify
+from neomodel import db
+
+def register_graph_routes(app, _):
+    @app.route('/api/relations/<node_type>', methods=['GET'])
+    def get_relation_types(node_type):
+        try:
+            query = f"""
+            MATCH (n:`{node_type}`)-[r]-()
+            RETURN DISTINCT type(r) AS relation_type
+            """
+
+            results, _ = db.cypher_query(query)
+
+            relations = [row[0] for row in results]
+
+            return jsonify({
+                'node_type': node_type,
+                'relation_types': relations
+            })
+
+        except Exception as e:
+            app.logger.exception("Relation type fetch error")
+            return jsonify({'error': str(e)}), 500
